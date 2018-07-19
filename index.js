dcecf2bdde2da229db0f1f5afc89a93ba8ef37f5
FedericoCozziVM <federicocozzivm@gmail.com>
2018-07-19 21:37:32 +0200

    index.js test modify

--- index.js
+++ index.js
@@ -18,12 +18,11 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
-      ? req.body.result.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+    fulfillmentText: speech,
+    source: "moviehint-webhook"
   });
 });
 
